2dc1a4e30c1ce87c4d1e9efb83056896fb6bf6c8
SimJJ96 <simjingjie13@gmail.com>
2021-02-19 17:18:27 +0800

    add new method in main

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -16,11 +16,10 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
-        System.out.println("Printing deadlines");
-        printDeadlines(tasksData);
 
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
-
+        printDeadLinesUsingStreams(tasksData);
+        System.out.println("Total number of deadlines (using stream): " +
+                countDeadLinesWithStreams(tasksData));
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
